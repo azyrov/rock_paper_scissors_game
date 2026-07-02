dfcc851d85f08a56e52aeb85d1941d5ca5239736
azyrov <for28@yahoo.com>
2026-07-02 07:40:33 +0000

    Add round logic

--- index.js
+++ index.js
@@ -29,11 +29,113 @@ switch (getComputerChoice()) {
         break;
 }
 
-// alert(getComputerChoiceString);
+console.log(`getComputerChoiceString: ${getComputerChoiceString}`);
 
 let getHumanChoice = prompt("Rock, Paper, Scissors? Choose ONE");
-console.log(getHumanChoice);
+
+console.log(`getHumanChoice: ${getHumanChoice}`);
 
 
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
+
+// console.log(`Computer: ${computerChoice}
+// Your Choice: ${humanChoice}
+
+let playRound = (humanChoice, computerChoice) => {
+    humanScore = 0;
+    computerScore = 0;
+
+    if (humanChoice === computerChoice) {
+        console.log(`Computer: ${computerChoice}
+        Your choice: ${humanChoice}.
+        Jinx! Try again.
+        
+        SCORE
+        Computer: ${computerScore}
+        You: ${humanScore};
+        `);
+        
+
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper"){
+            console.log(`Computer: ${computerChoice}
+            Your Choice: ${humanChoice}
+            You lose! Rock covers paper!
+            
+            SCORE
+            Computer: ${++computerScore}
+            You: ${humanScore}
+            `);
+            // console.log(++computerScore);
+
+        } else if (computerChoice === "scissors") {
+            console.log(`Computer: ${computerChoice}
+            Your Choice: ${humanChoice}
+            You win! Rock breaks Scissors! 
+
+            SCORE
+            Computer: ${computerScore}
+            You: ${++humanScore}
+            `);
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log(`Computer: ${computerChoice}
+            Your Choice: ${humanChoice}
+            You win! Paper covers Rock   
+
+            SCORE
+            Computer: ${computerScore}
+            You: ${++humanScore}
+            `);
+
+        } else if (computerChoice === "scissors") {
+            console.log(`Computer: ${computerChoice}
+            Your Choice: ${humanChoice}
+            You lose! Scissors cuts Paper!
+            
+                        
+            SCORE
+            Computer: ${++computerScore}
+            You: ${humanScore}
+            `);
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log(`Computer: ${computerChoice}
+            Your Choice: ${humanChoice}
+            You lose! Rock breaks Scissors!
+            
+                        
+            SCORE
+            Computer: ${++computerScore}
+            You: ${humanScore}
+            `);
+
+        } else if (computerChoice === "paper") {
+            console.log(`Computer: ${computerChoice}
+            Your Choice: ${humanChoice}
+            You win! Scissors cuts Paper!
+            
+                        
+            SCORE
+            Computer: ${computerScore}
+            You: ${++humanScore}
+            `);
+
+        }
+    } else {
+        alert(`Please input only one of the following:
+Rock
+Paper
+Scissors`);
+    }
+}
+
+let getHumanChoiceLowercase = getHumanChoice.toLowerCase();
+let computerChoice = getComputerChoiceString;
+
+playRound(getHumanChoiceLowercase, computerChoice);
+
+
